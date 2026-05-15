9ef4276ebcdbed170727dea2bfcfe118fda56456
Visihishtaa <visihishtaabalaji@gmail.com>
2026-05-15 10:57:32 +0530

    Final fix for model download

--- app.py
+++ app.py
@@ -10,16 +10,21 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropou
 
 # -------------------- SETTINGS --------------------
 MODEL_PATH = "model/mnist_model.keras"
-os.makedirs("model", exist_ok=True)
 
+# Create folders
+os.makedirs("model", exist_ok=True)
+os.makedirs("collected_mistakes", exist_ok=True)
+
+# Download model if not present
 if not os.path.exists(MODEL_PATH):
+    import gdown
+
     file_id = "1NR0U8hbNUv9wPvClKdQk_532UFa1GeLP"
     url = f"https://drive.google.com/uc?id={file_id}"
+
     gdown.download(url, MODEL_PATH, quiet=False)
 
 SAVE_DIR = "collected_mistakes"
-os.makedirs(SAVE_DIR, exist_ok=True)
-
 # -------------------- LOAD MODEL --------------------
 from tensorflow.keras.models import load_model
 
